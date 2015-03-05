beb5ed31308c6b16c0f52fbff69f4b832ebd2cd9
jthomm <jthomm@yahoo.com>
2015-03-05 00:48:41 -0500

    Add Yahoo! data files

--- AUTOLOAD.py
+++ AUTOLOAD.py
@@ -71,7 +71,7 @@ for folder in folders:
 
 
 """
-  CREATE VIEW v_mrcs80_stmr20 AS
+  CREATE VIEW v_bat_mrcs80_stmr20 AS
   SELECT t.fg_id AS fg_id,
          0.2*t.pa + 0.8*u.pa AS pa,
          0.2*t.ab/t.pa + 0.8*u.ab/u.pa AS ab_pa,
@@ -90,7 +90,7 @@ for folder in folders:
 ;
 
 
-  CREATE VIEW v_stmr65_zips35 AS
+  CREATE VIEW v_bat_stmr65_zips35 AS
   SELECT t.fg_id AS fg_id,
          0.1*t.pa + 0.9*u.pa AS pa,
          0.35*t.ab/t.pa + 0.65*u.ab/u.pa AS ab_pa,
@@ -108,6 +108,98 @@ for folder in folders:
    WHERE t.fg_id = u.fg_id
 ;
 
+  CREATE VIEW v_pit_mrcs80_stmr20 AS
+  SELECT t.fg_id AS fg_id,
+         0.2*t.ip + 0.8*u.ip AS ip,
+         0.2*t.w/t.ip + 0.8*u.w/u.ip AS w_ip,
+         0.2*t.sv/t.ip + 0.8*u.sv/u.ip AS sv_ip,
+         0.2*t.h/t.ip + 0.8*u.h/u.ip AS h_ip,
+         0.2*t.so/t.ip + 0.8*u.so/u.ip AS so_ip,
+         0.2*t.bb/t.ip + 0.8*u.bb/u.ip AS bb_ip,
+         0.2*t.er/t.ip + 0.8*u.er/u.ip AS er_ip
+    FROM pit_stmr t,
+         pit_mrcs u
+   WHERE t.fg_id = u.fg_id
+     AND t.ip > 1
+;
+
+  CREATE VIEW v_pit_stmr65_zips35 AS
+  SELECT t.fg_id AS fg_id,
+         0.1*t.ip + 0.9*u.ip AS ip,
+         0.35*t.w/t.ip + 0.65*u.w/u.ip AS w_ip,
+         1.0*u.sv/u.ip AS sv_ip,
+         0.35*t.h/t.ip + 0.65*u.h/u.ip AS h_ip,
+         0.35*t.so/t.ip + 0.65*u.so/u.ip AS so_ip,
+         0.35*t.bb/t.ip + 0.65*u.bb/u.ip AS bb_ip,
+         0.35*t.er/t.ip + 0.65*u.er/u.ip AS er_ip
+    FROM pit_zips t,
+         pit_stmr u
+   WHERE t.fg_id = u.fg_id
+     AND u.ip > 1
+;
+
+
+
+  SELECT fg_id,
+         ip,
+         ip*w_ip AS w,
+         ip*sv_ip AS sv,
+         ip*h_ip AS h,
+         ip*so_ip AS so,
+         ip*bb_ip AS bb
+    FROM (
+  SELECT IFNULL (t_fg_id, u_fg_id) AS fg_id,
+         IFNULL (t_ip, u_ip) AS ip,
+         IFNULL (t_w_ip, u_w_ip) AS w_ip,
+         IFNULL (t_sv_ip, u_sv_ip) AS sv_ip,
+         IFNULL (t_h_ip, u_h_ip) AS h_ip,
+         IFNULL (t_so_ip, u_so_ip) AS so_ip,
+         IFNULL (t_bb_ip, u_bb_ip) AS bb_ip
+    FROM (
+  SELECT t.fg_id AS t_fg_id,
+         t.ip AS t_ip,
+         t.w_ip AS t_w_ip,
+         t.sv_ip AS t_sv_ip,
+         t.h_ip AS t_h_ip,
+         t.so_ip AS t_so_ip,
+         t.bb_ip AS t_bb_ip,
+         u.fg_id AS u_fg_id,
+         u.ip AS u_ip,
+         u.w_ip AS u_w_ip,
+         u.sv_ip AS u_sv_ip,
+         u.h_ip AS u_h_ip,
+         u.so_ip AS u_so_ip,
+         u.bb_ip AS u_bb_ip
+    FROM v_pit_mrcs80_stmr20 t
+    LEFT
+   OUTER
+    JOIN v_pit_stmr65_zips35 u
+      ON t.fg_id = u.fg_id
+   UNION
+     ALL
+  SELECT t.fg_id AS t_fg_id,
+         t.ip AS t_ip,
+         t.w_ip AS t_w_ip,
+         t.sv_ip AS t_sv_ip,
+         t.h_ip AS t_h_ip,
+         t.so_ip AS t_so_ip,
+         t.bb_ip AS t_bb_ip,
+         u.fg_id AS u_fg_id,
+         u.ip AS u_ip,
+         u.w_ip AS u_w_ip,
+         u.sv_ip AS u_sv_ip,
+         u.h_ip AS u_h_ip,
+         u.so_ip AS u_so_ip,
+         u.bb_ip AS u_bb_ip
+    FROM v_pit_stmr65_zips35 u
+    LEFT
+   OUTER
+    JOIN v_pit_mrcs80_stmr20 t
+      ON u.fg_id = t.fg_id
+   WHERE t.fg_id IS NULL
+         )
+         )
+;
 
   SELECT fg_id,
          pa,
@@ -159,10 +251,10 @@ for folder in folders:
          u.bb_pa AS u_bb_pa,
          u.so_pa AS u_so_pa,
          u.sb_pa AS u_sb_pa
-    FROM v_mrcs80_stmr20 t
+    FROM v_bat_mrcs80_stmr20 t
     LEFT
    OUTER
-    JOIN v_stmr65_zips35 u
+    JOIN v_bat_stmr65_zips35 u
       ON t.fg_id = u.fg_id
    UNION
      ALL
@@ -190,10 +282,10 @@ for folder in folders:
          u.bb_pa AS u_bb_pa,
          u.so_pa AS u_so_pa,
          u.sb_pa AS u_sb_pa
-    FROM v_stmr65_zips35 u
+    FROM v_bat_stmr65_zips35 u
     LEFT
    OUTER
-    JOIN v_mrcs80_stmr20 t
+    JOIN v_bat_mrcs80_stmr20 t
       ON u.fg_id = t.fg_id
    WHERE t.fg_id IS NULL
          )
