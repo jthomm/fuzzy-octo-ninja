09e505a25c7d0270b8f23d54593d4159846192b3
jthomm <jthomm@yahoo.com>
2015-07-07 22:38:33 -0400

    It's been a long time

--- correl_table.py
+++ correl_table.py
@@ -1,3 +1,113 @@
+from connect import connect
+connection, cursor = connect('fbb')
+
+data = cursor.execute("""
+  SELECT t.*,
+         u.*
+    FROM foo t,
+         foo u
+   WHERE     t.fg_id = u.fg_id
+         AND t.year = u.year + 1
+         AND t.pa >= 200
+         AND u.pa >= 200
+""").fetchall()
+
+
+data = cursor.execute('''
+  SELECT t.*, u.*
+    FROM baz t, baz u
+   WHERE t.fg_id = u.fg_id
+     AND t.year = u.year - 1
+     AND t.pa >= 200
+     AND u.pa >= 200
+''').fetchall()
+
+import pandas as pd
+df = pd.DataFrame(data=data, columns=data[0].keys())
+
+import numpy as np
+from scipy.special import logit
+from scipy.stats import pearsonr
+
+def sum_of(df, columns):
+    return sum(df[column] for column in columns)
+
+def underscore(columns):
+    return [u'_{column}'.format(column=column) for column in columns]
+
+def logit_of(df, numer, denom):
+    return logit(sum_of(df, numer)/sum_of(df, denom))
+
+def pearsonr_of(df, numer, denom):
+    x = logit_of(df, numer, denom)
+    y = logit_of(df, underscore(numer), underscore(denom))
+    i = np.isfinite(x) & np.isfinite(y)
+    n = len(i)
+    r, p = pearsonr(x[i], y[i])
+    return (n, r, p,)
+
+def binary_subset(subset, superset):
+    return tuple(1 if item in subset else 0 for item in superset)
+
+import datetime
+
+def log(message):
+    print u'[{timestamp}] {message}'.format(
+        timestamp=datetime.datetime.now().isoformat(),
+        message=message)
+
+from itertools import combinations
+
+def run(number_of_denoms):
+    log('Working on {0} denoms'.format(number_of_denoms))
+    for denom in combinations(stats, number_of_denoms):
+        for number_of_numers in xrange(1, len(denom)):
+            for numer in combinations(denom, number_of_numers):
+                n, r, p = pearsonr_of(df, numer, denom)
+                yield (n, r, p, numer, denom,)
+
+stats = (
+    u'ubb',
+    u'ibb',
+    u'hbp',
+    u'so',
+    u'bu',
+    u'gb',
+    u'ld',
+    u'hrofb',
+    u'sofb',
+    u'ofb',
+    u'ifb',
+)
+
+stats = (
+    'ubb',
+    'ibb',
+    'hbp',
+    'so',
+    'bu_hard',
+    'bu_soft',
+    'gb_hard',
+    'gb_soft',
+    'ld_hr',
+    'ld_hard',
+    'ld_soft',
+    'offb_hr',
+    'offb_hard',
+    'offb_soft',
+    'iffb',
+)
+
+numer_cols = ['n_{0}'.format(stat) for stat in stats]
+denom_cols = ['d_{0}'.format(stat) for stat in stats]
+
+new_results = [r[:3] + binary_subset(r[3], stats) + binary_subset(r[4], stats) for r in results]
+fd = pd.DataFrame(data=new_results, columns=['n', 'r', 'p',] + numer_cols + denom_cols)
+fd['numer'] = sum(fd[col] for col in numer_cols)
+fd['denom'] = sum(fd[col] for col in denom_cols)
+
+
+"""
 from __future__ import division
 
 import sqlite3
@@ -30,12 +140,21 @@ cur = con.cursor()
 cur.execute('''
   SELECT t.*,
          u.*
-    FROM bar t,
-         bar u
+    FROM foo t,
+         foo u
    WHERE     t.fg_id = u.fg_id
          AND t.year = u.year + 1
          AND t.pa >= 400
          AND u.pa >= 400
+''')
+
+cur.execute('''
+  SELECT t.*, u.*
+    FROM baz t, baz u
+   WHERE t.fg_id = u.fg_id
+     AND t.year = u.year - 1
+     AND t.pa >= 200
+     AND u.pa >= 200
 ''')
 
 data = cur.fetchall()
@@ -52,6 +171,28 @@ stats = (
     'sofb',
     'ofb',
     'ifb',
+)
+
+stats = (
+    'ubb',
+    'ibb',
+    'hbp',
+    'so',
+    'bu_hard',
+    'bu_med',
+    'bu_soft',
+    'gb_hard',
+    'gb_med',
+    'gb_soft',
+    'ld_hr',
+    'ld_hard',
+    'ld_med',
+    'ld_soft',
+    'offb_hr',
+    'offb_hard',
+    'offb_med',
+    'offb_soft',
+    'iffb',
 )
 
 
@@ -139,3 +280,4 @@ def generate_all_combos(i, j):
             con.commit()
 
 generate_all_combos(2, 3)
+"""
